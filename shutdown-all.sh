af8d3daba1712939673b70c37dd98e0f4d0f2946
Attila Varga <attila.varga@outlook.hu>
2020-04-01 22:13:44 +0200

    shudown all bash script

---
+++ shutdown-all.sh
@@ -0,0 +1,16 @@
+cd cassandra
+sudo docker-compose down
+cd ..
+cd mongo
+sudo docker-compose down
+cd ..
+cd mysql
+sudo docker-compose down
+cd ..
+cd postgres
+sudo docker-compose down
+cd ..
+cd redis
+sudo docker-compose down
+cd ..
+exit
